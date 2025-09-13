dd328d3a2906b7a0a769886e2211af9c7bddc72f
cvm47 <shivamuniyal1810@gmail.com>
2025-09-13 16:49:32 +0530

    refactor: use cssText

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ let container = document.querySelector('.container')
 
 for (let i = 0; i < 16; i++) {
     let onlySixteenGrids = document.createElement('div')
-    onlySixteenGrids.style.flex = '1'
-    onlySixteenGrids.style.display = 'flex'
-    onlySixteenGrids.style.flexDirection = 'column'
+    onlySixteenGrids.style.cssText = "flex: 1; display: flex; flex-direction: column"
     for (let j = 0; j < 16; j++) {
         let onlyOneGrid = document.createElement('div')
-        onlyOneGrid.style.border = '1px solid #acacacff'
-        onlyOneGrid.style.borderRadius = '1px'
-        onlyOneGrid.style.flex = '1'
+        onlyOneGrid.style.cssText = "border: 1px solid #bababaff; border-radius: 2px; flex: 1"
         onlySixteenGrids.appendChild(onlyOneGrid)
     }
     container.appendChild(onlySixteenGrids)
