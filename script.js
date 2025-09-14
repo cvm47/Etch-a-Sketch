2a0fd748aa223d328cbfbb60864a6b345e98dc59
cvm47 <shivamuniyal1810@gmail.com>
2025-09-14 10:28:35 +0530

    refactor: change scope of only16Grids & only1Grid to fetch them from outside the loop

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // step 1
 let container = document.querySelector('.container')
 
+let onlySixteenGrids, onlyOneGrid;
 for (let i = 0; i < 16; i++) {
-    let onlySixteenGrids = document.createElement('div')
+    onlySixteenGrids = document.createElement('div')
     onlySixteenGrids.style.cssText = "flex: 1; display: flex; flex-direction: column"
     for (let j = 0; j < 16; j++) {
-        let onlyOneGrid = document.createElement('div')
+        onlyOneGrid = document.createElement('div')
         onlyOneGrid.style.cssText = "border: 1px solid #bababaff; border-radius: 2px; flex: 1"
         onlySixteenGrids.appendChild(onlyOneGrid)
     }
