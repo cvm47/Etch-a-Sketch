9060a759f640d53c2cddf6f98e19e593f55b3e9c
cvm47 <shivamuniyal1810@gmail.com>
2025-09-14 10:33:57 +0530

    feat: add class to each grid & querySelectorAll + mouseOver

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ for (let i = 0; i < 16; i++) {
     onlySixteenGrids.style.cssText = "flex: 1; display: flex; flex-direction: column"
     for (let j = 0; j < 16; j++) {
         onlyOneGrid = document.createElement('div')
+        onlyOneGrid.classList.add('bgColor')
         onlyOneGrid.style.cssText = "border: 1px solid #bababaff; border-radius: 2px; flex: 1"
         onlySixteenGrids.appendChild(onlyOneGrid)
     }
     container.appendChild(onlySixteenGrids)
 }
+
+// step 2 : hover
+let gridsBgColor = document.querySelectorAll('.bgColor')
+gridsBgColor.forEach((gridBgColor) => {
+    gridBgColor.addEventListener('mouseover',() => {
+        gridBgColor.style.background = 'pink'
+    })
+})
