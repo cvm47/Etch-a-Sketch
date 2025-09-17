520d05708ba4ffff84ec5e37574e67996f692fce
cvm47 <shivamuniyal1810@gmail.com>
2025-09-17 13:05:10 +0530

    refactor: remove JS cssText and use CSS classes

--- script.js
+++ script.js
@@ -14,11 +14,10 @@ function createGrid(size) {
     let column,cell;
     for (let i = 0; i < size; i++) {
         column = document.createElement('div')
-        column.style.cssText = "flex: 1; display: flex; flex-direction: column;"
+        column.classList.add('column')
         for (let j = 0; j < size; j++) {
             cell = document.createElement('div')
             cell.classList.add('cellBgColor')
-            cell.style.cssText = 'flex: 1; border: 1px solid #e3e3e3ff'
             column.appendChild(cell)
         }
         gridContainer.appendChild(column)
