dd545f7421002ece54ba3a2a886e5570baa8541b
cvm47 <shivamuniyal1810@gmail.com>
2025-09-17 09:38:03 +0530

    refactor: rename vairables and classes for better readability

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // step 1
 // let container = document.querySelector('.container')
 // step 3 :customGrids
-let customGrids = document.querySelector('.customGrids')
-let container = document.querySelector('.container')
+let gridSizeBtn = document.querySelector('.grid-size-btn')
+let gridContainer = document.querySelector('.grid-container')
 let totalGrids;
-customGrids.addEventListener('click', () => {
+gridSizeBtn.addEventListener('click', () => {
     totalGrids = +prompt('enter number of grid')
     // clearing container at every click
-    container.innerHTML = ''
+    gridContainer.innerHTML = ''
 
-    let onlySixteenGrids, onlyOneGrid;
+    let row, cell;
     for (let i = 0; i < totalGrids; i++) {
-        onlySixteenGrids = document.createElement('div')
-        onlySixteenGrids.style.cssText = "flex: 1; display: flex; flex-direction: column"
+        row = document.createElement('div')
+        row.style.cssText = "flex: 1; display: flex; flex-direction: column"
         for (let j = 0; j < totalGrids; j++) {
-            onlyOneGrid = document.createElement('div')
-            onlyOneGrid.classList.add('bgColor')
-            onlyOneGrid.style.cssText = "border: 1px solid #bababaff; border-radius: 2px; flex: 1"
-            onlySixteenGrids.appendChild(onlyOneGrid)
+            cell = document.createElement('div')
+            cell.classList.add('cellBgColor')
+            cell.style.cssText = "border: 0.5px solid #e3e3e3ff; border-radius: 1px; flex: 1"
+            row.appendChild(cell)
         }
-        container.appendChild(onlySixteenGrids)
+        gridContainer.appendChild(row)
     }
 
     // step 2 : hover
-    let gridsBgColor = document.querySelectorAll('.bgColor')
-    gridsBgColor.forEach((gridBgColor) => {
-        gridBgColor.addEventListener('mouseover',() => {
-            gridBgColor.style.background = 'pink'
+    let cells = document.querySelectorAll('.cellBgColor')
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover',() => {
+            cell.style.background = 'pink'
         })
     })
 
@@ -38,11 +38,11 @@ customGrids.addEventListener('click', () => {
         return `rgb(${red}, ${green}, ${blue})`
     }
 
-    let randomRGB_btn = document.querySelector('.randomRGB_btn')
-    randomRGB_btn.addEventListener('click',() => {
-        gridsBgColor.forEach((gridBgColor) => {
-            gridBgColor.addEventListener('mouseover', () => {
-                gridBgColor.style.background = getRandomRGB()
+    let randomColorBtn = document.querySelector('.random-color-btn')
+    randomColorBtn.addEventListener('click',() => {
+        cells.forEach((cell) => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.background = getRandomRGB()
             })
         })
     })
